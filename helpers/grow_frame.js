6ebea7bc29c04af2932f83f656bf95426206af50
Fabio Spampinato <spampinabio@gmail.com>
2017-07-26 03:27:16 +0200

    Fixed `grow` behaviour

--- helpers/grow_frame.js
+++ helpers/grow_frame.js
@@ -1,19 +1,23 @@
 
 /* GROW FRAME */
 
-function growFrame ( ...growth ) {
-
-  const window = Window.focused ();
+function growFrame ( x, y, width, height, window = Window.focused () ) {
 
   if ( !window ) return;
 
-  const frame = window.frame (),
-        newFrame = ['x', 'y', 'width', 'height'].reduce ( ( acc, key, index ) => {
-          return _.set ( acc, key, Math.max ( 0, frame[key] + growth[index] ) );
-        }, {} );
+  const screen = Screen.main (),
+        sFrame = screen.flippedFrame (),
+        svFrame = screen.flippedVisibleFrame (),
+        yUnusable = sFrame.height - svFrame.height,
+        frame = window.frame (),
+        newFrame = _.cloneDeep ( frame );
 
-  if ( growth[0] && frame.x === newFrame.x ) return;
-  if ( growth[1] && frame.y === newFrame.y ) return;
+  //FIXME: We are leveraging the fact that in the current use case `x` and `y` are always negatives, while `width` and `height` are always positives
+
+  if ( x ) newFrame.x = Math.max ( 0, frame.x + x );
+  if ( y ) newFrame.y = Math.max ( yUnusable, frame.y + y );
+  if ( width ) newFrame.width = Math.min ( svFrame.width - newFrame.x, frame.width + ( width - ( Math.abs ( x ) - Math.abs ( frame.x - newFrame.x ) ) ) );
+  if ( height ) newFrame.height = Math.min ( svFrame.height - ( newFrame.y - yUnusable ), frame.height + ( height - ( Math.abs ( y ) - Math.abs ( frame.y - newFrame.y ) ) ) );
 
   window.setFrame ( newFrame );
 
