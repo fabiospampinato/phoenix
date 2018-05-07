e7ef43ec7b3c393fed11db7b9304cfa86f0592e3
Fabio Spampinato <spampinabio@gmail.com>
2018-05-07 21:38:08 +0200

    Grow: fixed over growing

--- helpers/grow_frame.js
+++ helpers/grow_frame.js
@@ -16,8 +16,8 @@ function growFrame ( x, y, width, height, window = Window.focused () ) {
 
   if ( x ) newFrame.x = _.clamp ( frame.x + x, sFrame.x, sFrame.x + sFrame.width );
   if ( y ) newFrame.y = _.clamp ( frame.y + y, yUnusable, yUnusable + svFrame.height );
-  if ( width ) newFrame.width = Math.min ( svFrame.width, frame.width + ( width - ( Math.abs ( x ) - Math.abs ( frame.x - newFrame.x ) ) ) );
-  if ( height ) newFrame.height = Math.min ( svFrame.height, frame.height + ( height - ( Math.abs ( y ) - Math.abs ( frame.y - newFrame.y ) ) ) );
+  if ( width ) newFrame.width = Math.min ( x ? frame.x + frame.width : sFrame.width - frame.x, frame.width + ( width - ( Math.abs ( x ) - Math.abs ( frame.x - newFrame.x ) ) ) );
+  if ( height ) newFrame.height = Math.min ( y ? frame.y + frame.height : sFrame.height - frame.y, frame.height + ( height - ( Math.abs ( y ) - Math.abs ( frame.y - newFrame.y ) ) ) );
 
   window.setFrame ( newFrame );
 
