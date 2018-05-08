88c294aba43771ae24d116ec58f5ea2226c27b42
Fabio Spampinato <spampinabio@gmail.com>
2018-05-08 14:10:07 +0200

    findWindow: ignoring windows without a title
    This also works around the ghosty Finder windows bug.

--- helpers/find_window.js
+++ helpers/find_window.js
@@ -7,9 +7,12 @@ function findWindow ( windows = Window.all (), name = false, isNameOptional = fa
 
   for ( let i = 0, l = windows.length; i < l; i++ ) {
 
-    const window = windows[i];
+    const window = windows[i],
+          windowTitle = window.title ();
 
-    const titleOK = !title || ( title.test ( window.title () || '' ) );
+    if ( !windowTitle ) continue; // Not a normal window
+
+    const titleOK = !title || ( title.test ( windowTitle ) );
 
     if ( !titleOK ) continue;
 
