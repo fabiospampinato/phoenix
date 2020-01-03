cba7a899f6faa008a86ac74d137043cd01ff64d1
Vardan Pogosian <vardan.pogosyan@gmail.com>
2020-01-03 22:47:56 +0300

    fix(spaces): need to implementation

--- shortcuts/spaces.js
+++ shortcuts/spaces.js
@@ -1,6 +1,6 @@
 
 /* SPACES */
 
-(new EventDispatcher()).setHandler('tab', HYPER, () => new SpaceManager().switchSpace(1));
+// (new EventDispatcher()).setHandler('tab', HYPER, () => Space.active().next().active());
 
-(new EventDispatcher()).setHandler('tab', HYPER_SHIFT, () => new SpaceManager() / switchSpace(-1));
+// (new EventDispatcher()).setHandler('tab', HYPER_SHIFT, () => Space.active().previous().active());
