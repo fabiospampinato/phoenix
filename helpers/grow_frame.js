b61b3f36f9cc45e368b8f51ec22fdcd547f4d82d
Andrew Swerlick <andrew.swerlick@hbdi.com>
2020-02-09 21:19:04 -0500

    Fix problems with growFrame assuming screen origins at 0,0
    This commit fixes some issues with how growFrame sets bounds for the various
    frame parameters assuming that the top left corner of the screen is 0,0. This
    affects growing frames on secondary monitors in certain positions. The fix works
    by making sure that screen frame coordinates are properly inserted into
    calculations.

--- helpers/grow_frame.js
+++ helpers/grow_frame.js
@@ -15,10 +15,11 @@ function growFrame ( x, y, width, height, window = Window.focused () ) {
   //FIXME: We are leveraging the fact that in the current use case `x` and `y` are always negatives, while `width` and `height` are always positives
 
   if ( x ) newFrame.x = _.clamp ( frame.x + x, sFrame.x, sFrame.x + sFrame.width );
-  if ( y ) newFrame.y = _.clamp ( frame.y + y, yUnusable, yUnusable + svFrame.height );
-  if ( width ) newFrame.width = Math.min ( x ? frame.x + frame.width : sFrame.width - frame.x, frame.width + ( width - ( Math.abs ( x ) - Math.abs ( frame.x - newFrame.x ) ) ) );
-  if ( height ) newFrame.height = Math.min ( y ? frame.y + frame.height : sFrame.height - frame.y, frame.height + ( height - ( Math.abs ( y ) - Math.abs ( frame.y - newFrame.y ) ) ) );
+  if ( y ) newFrame.y = _.clamp ( frame.y + y, yUnusable + sFrame.y, yUnusable + sFrame.y + svFrame.height );
+  if ( width ) newFrame.width = Math.min ( x ? frame.x - sFrame.x + frame.width : sFrame.width - (frame.x - sFrame.x), frame.width + ( width - ( Math.abs ( x ) - Math.abs ( frame.x - newFrame.x ) ) ) );
+  if ( height ) newFrame.height = Math.min ( y ? frame.y + frame.height : sFrame.height - (frame.y - sFrame.y), frame.height + ( height - ( Math.abs ( y ) - Math.abs ( frame.y - newFrame.y ) ) ) );
 
+  log(sFrame.height, frame.y)
   window.setFrame ( newFrame );
 
 }
