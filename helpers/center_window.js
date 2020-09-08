56727f9bf342108a5f58dd4ecc5637471e22ab5c
Fabio Spampinato <spampinabio@gmail.com>
2020-09-08 14:30:25 +0100

    Ensuring windows are centered properly in external monitors too

--- helpers/center_window.js
+++ helpers/center_window.js
@@ -4,12 +4,12 @@
 function center_window ( window ) {
 
   const screen = window.screen (),
-        sFrame = screen.frame (),
+        sFrame = screen.flippedFrame (),
         wFrame = window.frame ();
 
   window.setFrame ({
     x: sFrame.x + ( sFrame.width / 2 ) - ( wFrame.width / 2 ),
-    y: Math.max ( 0, sFrame.y ) + ( sFrame.height / 2 ) - ( wFrame.height / 2 ),
+    y: sFrame.y + ( sFrame.height / 2 ) - ( wFrame.height / 2 ),
     width: wFrame.width,
     height: wFrame.height
   });
