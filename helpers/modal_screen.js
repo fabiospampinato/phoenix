92e41c3533039fc46dfaf78916eb8c2c9a203f3f
Fabio Spampinato <spampinabio@gmail.com>
2020-09-08 20:23:36 +0100

    Ensuring centered modals are centered in external monitors too

--- helpers/modal_screen.js
+++ helpers/modal_screen.js
@@ -3,12 +3,12 @@
 
 function modalScreen ( options, screen = getFocusedScreen () ) {
 
-  const frame = screen.flippedFrame ();
+  const frame = screen.frame ();
 
   options.origin = function ( mFrame ) {
     return {
       x: frame.x + ( frame.width / 2 ) - ( mFrame.width / 2 ),
-      y: ( frame.height / 2 ) - ( mFrame.height / 2 )
+      y: frame.y + ( frame.height / 2 ) - ( mFrame.height / 2 )
     };
   };
 
